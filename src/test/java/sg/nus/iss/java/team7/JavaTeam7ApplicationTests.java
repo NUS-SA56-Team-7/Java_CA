3404d186c9b13d029f4032ce586cec8a599c422d
nyi-97 <konnhtetnaing@gmail.com>
2023-06-19 17:39:58 +0800

    Initial Commit

---
+++ src/test/java/sg/nus/iss/java/team7/JavaTeam7ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.java.team7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTeam7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
