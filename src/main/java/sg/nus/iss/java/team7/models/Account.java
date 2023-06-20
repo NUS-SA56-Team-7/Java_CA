612976a2cd034cce7431f6023d1c4c08a90748b3
YSKabcdef <leroytantingen@gmail.com>
2023-06-20 22:53:55 +0800

    adding password bcrypt encoder for now

--- src/main/java/sg/nus/iss/java/team7/models/Account.java
+++ src/main/java/sg/nus/iss/java/team7/models/Account.java
@@ -8,20 +8,71 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 
-import lombok.Data;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @Entity
 @Inheritance
 @DiscriminatorColumn(name = "account_type")
-@Data
+
 public abstract class Account {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int account_id;
     @Column(columnDefinition="VARCHAR(45) NOT NULL")
     private String email;
-    @Column(columnDefinition="BINARY(32) NOT NULL")
+    @Column(columnDefinition="VARCHAR(45) NOT NULL")
     private String password;
+
+    public int getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(int account_id) {
+        this.account_id = account_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodepassword = encoder.encode(password);
+        this.password = encodepassword;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
 
     @Column(columnDefinition="VARCHAR(45) NOT NULL")
     private String first_name;
--- src/main/java/sg/nus/iss/java/team7/models/Lecturer.java
+++ src/main/java/sg/nus/iss/java/team7/models/Lecturer.java
@@ -4,13 +4,23 @@ import java.util.List;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+
 
 @Entity
 @DiscriminatorValue("lecturer")
 public class Lecturer extends Account {
+
+
+public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
 
 
 @OneToMany
