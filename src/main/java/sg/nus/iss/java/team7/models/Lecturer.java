9f435590d67b0b6f9f3ba309b277076c6d2522af
YSKabcdef <leroytantingen@gmail.com>
2023-06-21 19:53:03 +0800

    changing the name of Id

--- src/main/java/sg/nus/iss/java/team7/models/Lecturer.java
+++ src/main/java/sg/nus/iss/java/team7/models/Lecturer.java
@@ -9,7 +9,6 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 
-import org.hibernate.annotations.CollectionId;
 
 import lombok.NoArgsConstructor;
 
