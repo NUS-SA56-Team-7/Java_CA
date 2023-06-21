01b2bf124bf564386d9d77d0fd22781dd3314abd
YSKabcdef <leroytantingen@gmail.com>
2023-06-21 09:34:33 +0800

    Merge branch 'leroy' of https://github.com/NUS-SA56-Team-7/Java_CA into leroy

--- src/main/java/sg/nus/iss/java/team7/controllers/ErrorController.java
+++ src/main/java/sg/nus/iss/java/team7/controllers/ErrorController.java
@@ -7,12 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ErrorController {
     @RequestMapping(path="/error",method=RequestMethod.POST)
-    public Map<String, Object> handle(HttpServletRequest request)
+    public @ResponseBody Map<String, Object> handle(HttpServletRequest request)
     {
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("status", request.getAttribute("javax.servlet.error.status_code"));
