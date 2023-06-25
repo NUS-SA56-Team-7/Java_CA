8638828d99975e5fad1dddb732ceccd0cab7c490
YOHAHDS <130519654+YOHAHDS@users.noreply.github.com>
2023-06-25 16:39:23 +0800

    Update StudentServiceImpl.java

--- src/main/java/sg/nus/iss/java/team7/services/implementations/StudentServiceImpl.java
+++ src/main/java/sg/nus/iss/java/team7/services/implementations/StudentServiceImpl.java
@@ -1,5 +1,48 @@
 package sg.nus.iss.java.team7.services.implementations;
 
-public class StudentServiceImpl {
+import java.util.List;
 
-}
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import sg.nus.iss.java.team7.models.Student;
+import sg.nus.iss.java.team7.repos.StudentRepository;
+import sg.nus.iss.java.team7.services.StudentService;
+@Service
+public class StudentServiceImpl implements StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+    @Override
+    public List<Student> findAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student findStudentById(String id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Student> findStudentsByName(String name) {
+        return studentRepository.findByName(name);
+    }
+
+    @Override
+    public List<Student> findStudentsByMatricNumber(String matricNumber) {
+        return studentRepository.findByMatricNumber(matricNumber);
+    }
+
+    @Override
+    public List<Student> findStudentsByCourseId(String courseId) {
+        return studentRepository.findByCourseId(courseId);
+    }
+
+    @Override
+    public void saveOrUpdateStudent(Student student) {
+        studentRepository.save(student);
+    }
+
+    @Override
+    public void deleteStudentById(String id) {
+        studentRepository.deleteById(id);
+    }
