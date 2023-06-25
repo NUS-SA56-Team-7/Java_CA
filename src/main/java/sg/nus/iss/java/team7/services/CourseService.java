3323273586382c516518327665d5fd5dbfd00ca5
YOHAHDS <130519654+YOHAHDS@users.noreply.github.com>
2023-06-25 16:37:29 +0800

    Update CourseService.java

--- src/main/java/sg/nus/iss/java/team7/services/CourseService.java
+++ src/main/java/sg/nus/iss/java/team7/services/CourseService.java
@@ -1,5 +1,25 @@
 package sg.nus.iss.java.team7.services;
 
-public class CourseService {
+import java.util.List;
 
+import sg.nus.iss.java.team7.models.Course;
+
+public interface CourseService {
+
+    List<Course> findAllCourses();
+
+
+    Course findCourseById(String id);
+
+
+    Course findCourseByName(String name);
+
+
+    List<Course> findCoursesByLecturerId(String lecturerId);
+
+
+    void saveOrUpdateCourse(Course course);
+
+
+    void deleteCourseById(String id);
 }
