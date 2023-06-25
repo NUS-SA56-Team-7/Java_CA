858b704f6ce3f624fef3a8de38037cc247e3a2e7
YOHAHDS <130519654+YOHAHDS@users.noreply.github.com>
2023-06-25 16:35:13 +0800

    Update CourseRepository.java

--- src/main/java/sg/nus/iss/java/team7/repos/CourseRepository.java
+++ src/main/java/sg/nus/iss/java/team7/repos/CourseRepository.java
@@ -1,5 +1,16 @@
 package sg.nus.iss.java.team7.repos;
 
-public class CourseRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import sg.nus.iss.java.team7.models.Course;
+
+public interface CourseRepository extends JpaRepository<Course,Long> {
+    @Query("SELECT c FROM Course c WHERE c.course_name LIKE :CourseName")
+    public Course findByCourseName(@Param("CourseName")String name);
+
+    //Course findByName(String name);
+
+    List<Course> findByLecturerId(String lecturerId);
 }
