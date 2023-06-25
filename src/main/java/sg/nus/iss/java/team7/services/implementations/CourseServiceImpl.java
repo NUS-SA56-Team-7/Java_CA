bcae4ef0366143447f9aaf13e2aa45a94e0e249f
YOHAHDS <130519654+YOHAHDS@users.noreply.github.com>
2023-06-25 16:39:06 +0800

    Update CourseServiceImpl.java

--- src/main/java/sg/nus/iss/java/team7/services/implementations/CourseServiceImpl.java
+++ src/main/java/sg/nus/iss/java/team7/services/implementations/CourseServiceImpl.java
@@ -1,5 +1,43 @@
 package sg.nus.iss.java.team7.services.implementations;
 
-public class CourseServiceImpl {
+import java.util.List;
 
-}
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import sg.nus.iss.java.team7.models.Course;
+import sg.nus.iss.java.team7.repos.CourseRepository;
+import sg.nus.iss.java.team7.services.CourseService;
+@Service
+public class CourseServiceImpl implements CourseService {
+    @Autowired
+    private CourseRepository courseRepository;
+    @Override
+    public List<Course> findAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    @Override
+    public Course findCourseById(String id) {
+        return courseRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Course findCourseByName(String name) {
+        return courseRepository.findByName(name);
+    }
+
+    @Override
+    public List<Course> findCoursesByLecturerId(String lecturerId) {
+        return courseRepository.findByLecturerId(lecturerId);
+    }
+
+    @Override
+    public void saveOrUpdateCourse(Course course) {
+        courseRepository.save(course);
+    }
+
+    @Override
+    public void deleteCourseById(String id) {
+        courseRepository.deleteById(id);
+    }
