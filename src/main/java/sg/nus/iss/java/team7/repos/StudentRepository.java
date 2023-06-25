3858bc2d76bf1c6b5083ce64349e5b40c29644fe
YOHAHDS <130519654+YOHAHDS@users.noreply.github.com>
2023-06-25 16:35:41 +0800

    Update StudentRepository.java

--- src/main/java/sg/nus/iss/java/team7/repos/StudentRepository.java
+++ src/main/java/sg/nus/iss/java/team7/repos/StudentRepository.java
@@ -1,5 +1,18 @@
 package sg.nus.iss.java.team7.repos;
 
-public class StudentRepository {
 
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import sg.nus.iss.java.team7.models.Student;
+public interface StudentRepository extends JpaRepository<Student,Long> {
+    @Query(value = "Select s FROM Student s WHERE matric_number LIKE %:matricNumber%")
+    public List<Student> findByMatricNumber(@Param("matricNumber") String matricNumber);
+
+    @Query("select s from Student s join s.courses c where c.id = :courseId")
+    List<Student> findByCourseId(@Param("courseId") String courseId);
 }
