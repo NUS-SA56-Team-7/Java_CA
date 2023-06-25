293284644c1a4003c8165eac1b5e9f9a481ae253
YOHAHDS <130519654+YOHAHDS@users.noreply.github.com>
2023-06-25 16:37:52 +0800

    Update StudentService.java

--- src/main/java/sg/nus/iss/java/team7/services/StudentService.java
+++ src/main/java/sg/nus/iss/java/team7/services/StudentService.java
@@ -1,5 +1,28 @@
 package sg.nus.iss.java.team7.services;
 
-public class StudentService {
+import java.util.List;
 
+
+import sg.nus.iss.java.team7.models.Student;
+public interface StudentService {
+
+    List<Student> findAllStudents();
+
+
+    Student findStudentById(String id);
+
+
+    List<Student> findStudentsByName(String name);
+
+
+    List<Student> findStudentsByMatricNumber(String matricNumber);
+
+
+    List<Student> findStudentsByCourseId(String courseId);
+
+
+    void saveOrUpdateStudent(Student student);
+
+
+    void deleteStudentById(String id);
 }
